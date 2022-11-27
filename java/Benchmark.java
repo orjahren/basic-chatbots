d1d328b8c49873376b3cb88a06ff3ecc263faaad
Oliver Jahren <oliverrj@ifi.uio.no>
2022-11-27 23:22:09 +0100

    Init benchmark class

---
+++ java/Benchmark.java
@@ -0,0 +1,15 @@
+class Benchmark {
+    private void testSequential() {
+
+    }
+
+    private void testConcurrent() {
+
+    }
+
+    public static void main(String[] args) {
+        Benchmark ben = new Benchmark();
+        ben.testSequential();
+        ben.testConcurrent();
+    }
+}
